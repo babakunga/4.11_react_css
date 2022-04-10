d4ca8b221ee9164f33b87f67145c5ce3a9bce5ee
T.Inagaki <101966091+babakunga@users.noreply.github.com>
2022-04-11 06:35:32 +0900

    Initial commit

---
+++ src/components/StyledComponents.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const StyledComponents = () => {
+  return (
+    <>
+      <Container>
+        <Title>- StyledComponents -</Title>
+        <Button>ファイト！</Button>
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.div`
+  border: solid 2px #392eff;
+  border-radius: 20px;
+  padding: 8px;
+  margin: 8px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Title = styled.p`
+  margin: 0;
+  color: #3d84a8;
+`;
+
+const Button = styled.button`
+  background-color: #abedd8;
+  border: none;
+  border-radius: 8px;
+  padding: 8px;
+  &:hover {
+    background-color: #46cdcf;
+    color: #fff;
+    cursor: pointer;
+  }
+`;
